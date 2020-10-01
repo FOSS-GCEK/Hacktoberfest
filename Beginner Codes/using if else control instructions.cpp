a28cc309fc9ed95487b1ff8897ddb4573672c971
PRANAV SURESH <69470563+pranav2001-02@users.noreply.github.com>
2020-10-01 11:50:00 +0530

    Update using if else control instructions.cpp

--- Beginner Codes/using if else control instructions.cpp
+++ Beginner Codes/using if else control instructions.cpp
@@ -1,17 +1,12 @@
-#include <stdio.h>
-
+#include<bits/stdc++.h>
 int main()
 {
   int n;
-
   printf("Enter a number\n");
-  scanf("%d", &n);
-
+  cin>>n;
   if (n > 0)
-    printf("Greater than zero.\n");
+     printf("Greater than zero.\n");
   else
-    printf("Less than or equal to zero.\n");
-
+     printf("Less than or equal to zero.\n");
   return 0;
 }
-
