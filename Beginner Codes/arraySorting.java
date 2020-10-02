cdd273ed700496b7e205e617c14ba7c9b88c633d
sidharth-a <56468787+sidharth-a@users.noreply.github.com>
2020-10-02 20:49:47 +0530

    Created a method for sorting :)

--- Beginner Codes/arraySorting.java
+++ Beginner Codes/arraySorting.java
@@ -8,7 +8,6 @@ public class JavaExample
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter number of elements you want in the array: ");
         count = scan.nextInt();
-    
         int num[] = new int[count];
         System.out.println("Enter array elements:");
         for (int i = 0; i < count; i++) 
@@ -16,22 +15,26 @@ public class JavaExample
             num[i] = scan.nextInt();
         }
         scan.close();
-        for (int i = 0; i < count; i++) 
+        Sort(num, count);
+    }
+    static void Sort(int arr[], int n){
+        int temp;
+        for (int i = 0; i < n; i++) 
         {
-            for (int j = i + 1; j < count; j++) { 
-                if (num[i] > num[j]) 
+            for (int j = i + 1; j < n; j++) { 
+                if (arr[i] > arr[j]) 
                 {
-                    temp = num[i];
-                    num[i] = num[j];
-                    num[j] = temp;
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
         System.out.print("Array Elements in Ascending Order: ");
-        for (int i = 0; i < count - 1; i++) 
+        for (int i = 0; i < n - 1; i++) 
         {
-            System.out.print(num[i] + ", ");
+            System.out.print(arr[i] + ", ");
         }
-        System.out.print(num[count - 1]);
+        System.out.print(arr[n - 1]);
     }
 }
