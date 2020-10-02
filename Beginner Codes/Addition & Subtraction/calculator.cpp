b81280d61788b528ac0c0863e45e92ed07668db8
Sayan Manna <53704744+Sayan-Manna@users.noreply.github.com>
2020-10-02 23:55:31 +0530

    Addition and Subtraction Added

---
+++ Beginner Codes/Addition & Subtraction/calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+using namespace std; 
+
+main() 
+{ 
+	char op; 
+	float num1, num2; 
+	cin >> op; 
+	cin >> num1 >> num2; 
+	switch (op) { 
+		case '+': 
+			cout << num1 + num2; 
+			break; 
+		case '-': 
+			cout << num1 - num2; 
+			break; 
+
+		default: 
+			cout << "Error! operator is not included"; 
+			break; 
+	}
+
+	return 0; 
+} 
---
+++ Beginner Codes/Addition & Subtraction/calculator.py
@@ -0,0 +1,20 @@
+# cook your dish here
+def Addition(a, b):
+    return a + b
+def Subtraction(a, b):
+    return a - b 
+   
+num1 = int(input())
+num2 = int(input())
+print(Addition(num1, num2))
+print(Subtraction(num1, num2))
+
+"""
+Input: 
+20
+10
+
+OutPut:
+30
+10
+"""
