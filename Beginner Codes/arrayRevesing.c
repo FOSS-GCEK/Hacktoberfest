fe1cc70b40c5585fadc785133ffe2e8579f9127a
sidharth-a <56468787+sidharth-a@users.noreply.github.com>
2020-10-02 20:04:24 +0530

    Memory allocation changed from static to dynamic:)

--- Beginner Codes/arrayRevesing.c
+++ Beginner Codes/arrayRevesing.c
@@ -1,14 +1,16 @@
-#include <stdio.h>    
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()    
 {   int n,i;   
-    int arr[n];  
+    int *arr;  
     printf("\nInput the size of array: ");  
     scanf("%d",&n); 
+    arr = malloc(n*sizeof(int));
     printf("\nEnter array elements:\n");
     for(i=0;i<n;i++)
     {
-      scanf("%d",&arr[i]);
+      scanf("%d",(arr+i));
     }
     printf(" \nEntered elements are:\n ");    
     for (i = 0; i < n; i++) 
