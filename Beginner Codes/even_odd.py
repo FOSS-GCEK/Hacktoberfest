7720b6bd5925f1e17be4815e64bd6e8ae8722052
Malavika_999 <65767896+malavika1699@users.noreply.github.com>
2020-10-03 18:23:19 +0530

    Update even_odd.py

--- Beginner Codes/even_odd.py
+++ Beginner Codes/even_odd.py
@@ -1,9 +1,9 @@
 num = int(input("Enter a number: "))
 f=1
-if(n==0):
+if(num==0):
     print("Factorial is zero")
 else:
-    for i in range(1,n+1):
+    for i in range(1,num+1):
         f=f*i
     print("Factorial is {0}".format(f))
 if (num % 2) == 0:
