06d6e7fc415a1f1e694ae71c1773c0feb8555394
Malavika_999 <65767896+malavika1699@users.noreply.github.com>
2020-10-04 12:09:13 +0530

    Update to check prime no.py

--- Beginner Codes/to check prime no.py
+++ Beginner Codes/to check prime no.py
@@ -1,6 +1,6 @@
 #logic - by using a additional variable and checking condition on it
 
-num = int(input("Enter a no to check Prime no."))
+num = int(input("Enter a number."))
 n = 0
 for i in range(2,num):
 	if(num % i == 0):
@@ -9,3 +9,8 @@ for i in range(2,num):
 		break	
 if(n == 0):
 	print("its a prime no.")
+	
+celsius = int(input("Enter temp."))
+
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
