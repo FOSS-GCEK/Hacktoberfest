17ffaf4dc1b3c75e6a1db87501370087e59d8e3d
Awar Rupa <48853554+awarrupa8@users.noreply.github.com>
2020-10-15 21:41:24 +0530

    Update reverse_a_string.py
    tried to reduce time complexity by using builtin method and simple appproch of string in python

--- Beginner Codes/reverse_a_string.py
+++ Beginner Codes/reverse_a_string.py
@@ -7,3 +7,7 @@ for i in range(-1,-(length+1),-1):
     reverse_list.append(string_list[i])
 reverse=rev_str.join(reverse_list)
 print(reverse)
+
+#another approach
+string=intput("Enter any string: ")
+print(s[::-1])
