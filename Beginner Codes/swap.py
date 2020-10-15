e116001092b759cc26a0db3c1e73c58f68bb2260
Awar Rupa <48853554+awarrupa8@users.noreply.github.com>
2020-10-15 21:49:13 +0530

    Update swap.py
    without temporary varible swapping two values

--- Beginner Codes/swap.py
+++ Beginner Codes/swap.py
@@ -9,3 +9,12 @@ y = temp
 print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
 print('The sum of x and y: {}'.format(x+y))
+
+#another approach for swap without temrarory variable
+a=int(input("Enter a value: "))
+b=int(input("Enter b value: "))
+print("Before UpdateA :{0} B:{1}".format(a,b))
+a=a+b
+b=a-b
+a=a-b
+print("After update A :{0} B:{1}".format(a,b))
