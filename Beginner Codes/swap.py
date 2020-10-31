15deb1d4b45cfae68ddfb1d961cea49e3cee45c2
ankita080208 <52229554+ankita080208@users.noreply.github.com>
2020-10-31 22:22:37 +0530

    Update swap.py
    added another method (without third variable) and few comments too.

--- Beginner Codes/swap.py
+++ Beginner Codes/swap.py
@@ -1,11 +1,23 @@
-x = 5
-y = 10
-
+#METHOD -1 ( WITH THIRD VARIABLE)..
+#take inputs from user.
+x =int(input("enter the first number = ") 
+y =int(input("enter the second number = ") 
+#now take the new variable , do following squence.
 temp = x
 x = y
 y = temp
-
-
+#print the result after swapping and its sum.
 print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
 print('The sum of x and y: {}'.format(x+y))
+#end of file.
+       
+#METHOD-2(WITHOUT THIRD VARIABLE ).
+#take inputs from user.
+x =int(input("enter the first number = ") 
+y =int(input("enter the second number = ") 
+x = x + y  
+y = x - y  
+x = x - y  
+print("After Swapping: x =", x, " y =", y); 
+#end of code.
