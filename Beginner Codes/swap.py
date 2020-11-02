cc7c609f98c04c7a663e0872d17f102fd230e85f
ankita080208 <52229554+ankita080208@users.noreply.github.com>
2020-11-02 20:19:33 +0530

    Update swap.py
    added another swapping method without using third variable.

--- Beginner Codes/swap.py
+++ Beginner Codes/swap.py
@@ -1,11 +1,19 @@
-x = 5
-y = 10
-
+#METHOD-1 USING THIRD VARIABLE.
+x = int(("Enter the number1 = "))
+y = int(input("Enetr the number2 = "))
 temp = x
 x = y
 y = temp
-
-
 print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
 print('The sum of x and y: {}'.format(x+y))
+
+#METHOD-2 WITHOUT USING THIRD VARIABLE.
+x = int(input("Enter the number1 = "))
+y = int(input("Enetr the number2 = "))
+x = x + y 
+y = x - y
+x = x - y
+print('The value of number1 after swapping: {}'.format(x))
+print('The value of number2 after swapping: {}'.format(y))
+#END OF CODE.
